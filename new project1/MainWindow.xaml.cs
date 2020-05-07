10a9b241b5142b16b53ef1199c538b1cf5477c6a
Dawid <Dawid@DAWID-PC>
2020-05-07 20:06:46 +0200

    Add new files

---
+++ new project1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Windows;
+
+namespace new_project1
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();       
+        }
+        public class Album
+        {
+            public string nazwa { get; set; }
+            public string autor { get; set; }
+            public string rok { get; set; }
+            public string liczba { get; set; }
+
+        }
+ 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Album first = new Album();
+            first.nazwa = txtAlbum.Text;
+            first.autor = txtAutor.Text;
+            first.rok = txtRok.Text;
+            first.liczba = txtLiczba.Text;
+
+             gridTab.Items.Add(first);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtAlbum.Text = "";
+            txtAutor.Text = "";
+            txtLiczba.Text = "";
+            txtRok.Text = "";
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void gridTab_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string nazwa = txtAlbum.Text;
+            string autor = txtAutor.Text;
+            string rok = txtLiczba.Text;
+            string liczba = txtRok.Text;
+        }
+
+       
+
+     
+    }
+}
